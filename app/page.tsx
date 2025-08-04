a84ccd6a4eb05876a6f97abb57e3e6dfdfb96b98
Seungil <seungilbang@khu.ac.kr>
2025-08-04 14:14:42 +0900

    feat: Image components Optimizing

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -30,6 +31,21 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          {/* 레이아웃 이동을 피하기 위해 이미지의 너비와 높이를 설정하는 것이 좋으며, 이는 소스 이미지와 동일한 가로 세로 비율이어야 합니다.*/}
+          <Image 
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image 
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
